525fc2f7413724407785e36ba2c1bedf751bd523
Daniel Tavakoli <daniel.tavakoli1@gmail.com>
2017-11-13 21:05:51 +0100

    first commit

---
+++ SimplicityStudio/v4_workspace/soc-thunderboard-sense-2/GNU ARM v4.9.3 - Default/application_properties.d
@@ -0,0 +1,7 @@
+application_properties.o: ../application_properties.c \
+ C:\Users\danie\SimplicityStudio\v4_workspace\soc-thunderboard-sense-2\platform\bootloader\api/application_properties.h \
+ C:\Users\danie\SimplicityStudio\v4_workspace\soc-thunderboard-sense-2\protocol\bluetooth_2.6\ble_stack\inc\soc/bg_version.h
+
+C:\Users\danie\SimplicityStudio\v4_workspace\soc-thunderboard-sense-2\platform\bootloader\api/application_properties.h:
+
+C:\Users\danie\SimplicityStudio\v4_workspace\soc-thunderboard-sense-2\protocol\bluetooth_2.6\ble_stack\inc\soc/bg_version.h:
---
+++ SimplicityStudio/v4_workspace/soc-thunderboard-sense-2/timedelay.h
@@ -0,0 +1,8 @@
+#ifndef __TIMEDELAY_H_
+#define __TIMEDELAY_H_
+
+#include <stdint.h>
+
+void delayforfaen(int number_of_seconds);
+
+#endif
